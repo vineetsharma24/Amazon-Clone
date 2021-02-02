28fe4e2f714c3f4358a82652fbeca41065be2f0b
Vineet <sharma24vineet@gmail.com>
2021-02-02 09:24:36 +0530

    till static basket

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import React from "react";
 import './App.css';
+import Header from './Header';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>yes vinnet lets get started</h1>
-      {/* header */}
+      <Header></Header>
       {/*home*/}
     </div>
   );
