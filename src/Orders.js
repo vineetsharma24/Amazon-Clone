7de59b79ed1315e628374273900010875f7ce8bc
Vineet <sharma24vineet@gmail.com>
2021-02-10 12:02:29 +0530

    final order page 1st time

--- src/Orders.js
+++ src/Orders.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Orders.css';
 function Orders() {
     return (
-        <div className='orders'>
-            <h1>Your Orders</h1>
 
+        <div className="jumbotron__hole">
+            <h1 className="display__name">Thank You!</h1>
+            <h1 className="display__name">Your Order Has Been Successfully Placed.</h1>
+            <p className="lead__name"><strong>Please check your email</strong> and stay updated with delivery of your item</p>
+            <div className="lead__name">
+                <a className="btn__set" href="/" role="button">Continue Shopping</a>
+            </div>
         </div>
     )
 }
